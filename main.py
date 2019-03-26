7f4f5c808fe0735bcb35883a97cb096b68caef7e
prl900 <p.rozas.larraondo@gmail.com>
2019-03-26 21:36:44 +1100

    Tested version serving static NDVI image

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import request as freq
 from flask import send_file
 import numpy as np
 import numexpr as ne
+import urllib
 
 import io
 import matplotlib.pyplot as plt
@@ -15,12 +16,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     contents = urllib.request.urlopen("https://geoarray-dot-wald-1526877012527.appspot.com/geoarray?height=256&width=256&band=2&bbox=0.0,0.0,10.0,10.0").read()
-    nir = np.frombuffer(contents, dtype=np.uint8).reshape((256,256))
+    nir = np.frombuffer(contents, dtype=np.uint8).reshape((256,256)).astype(np.float32)
 
     contents = urllib.request.urlopen("https://geoarray-dot-wald-1526877012527.appspot.com/geoarray?height=256&width=256&band=1&bbox=0.0,0.0,10.0,10.0").read()
-    red = np.frombuffer(contents, dtype=np.uint8).reshape((256,256))
+    red = np.frombuffer(contents, dtype=np.uint8).reshape((256,256)).astype(np.float32)
 
-    ndvi = (nir - red) / (nir + red)
+
+    ndvi = "(nir - red) / (nir + red)"
     res = ne.evaluate(ndvi)
     
     out = io.BytesIO()
