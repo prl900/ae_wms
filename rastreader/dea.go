49594d4cf7d56b44779dc93c22faf72d30f92086
prl900 <pablo.larraondo@anu.edu.au>
2020-06-30 13:31:17 +0000

    Tested with cloud storage

--- rastreader/dea.go
+++ rastreader/dea.go
@@ -3,7 +3,6 @@ package rastreader
 import (
 	"fmt"
 	"image"
-	"image/png"
 	"io/ioutil"
 	"math"
 	"sync"
@@ -19,81 +18,74 @@ import (
 	"github.com/terrascope/scimage/scicolor"
 
 	"github.com/golang/snappy"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
 )
 
 const (
 	//tileName = "/home/p_rozas_larraondo/data/fc_metrics_WCF_%+04d_%+04d_l%d_%d.snp"
-	tileName = "/home/p_rozas_larraondo/data/irr_water_%+04d_%+04d_l%d.snp"
-	gda94    = "+proj=aea +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=132 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs "
+	//tileName = "/home/p_rozas_larraondo/data/irr_water_%+04d_%+04d_l%d.snp"
+	bucketName = "wald-wms"
+	tileName = "irr_water_%+04d_%+04d_l%d.snp"
 )
 
-func DrillTile(x, y, level int, poly *geometry.Polygon, wg *sync.WaitGroup) error {
+func DrillTile(x, y, level int, poly *geometry.Polygon, layer Layer, wg *sync.WaitGroup) (Stat, error) {
 	defer wg.Done()
 
+	stat := Stat{}
 
 	tileStep := (1 << level)
-	tileCov := proj4go.Coverage{BoundingBox: geo.BBox(float64(x)*1e4, float64(y-tileStep)*1e4, float64(x+tileStep)*1e4, float64(y)*1e4), Proj4: gda94}
-	fmt.Println(tileCov)
+	tileCov := proj4go.Coverage{BoundingBox: geo.BBox(float64(x)*1e4, float64(y-tileStep)*1e4, float64(x+tileStep)*1e4, float64(y)*1e4), Proj4: layer.Proj4}
 
 	fName := fmt.Sprintf(tileName, x, y, level)
-	fmt.Println(fName)
 	r, err := os.Open(fName)
 	if err != nil {
 		fmt.Println(fName, ": not found")
-		return nil
+		return stat, nil
 	}
 
-	fmt.Println("processing: ", fName)
 	cdata, err := ioutil.ReadAll(r)
 	r.Close()
 	if err != nil {
-		return fmt.Errorf("Error reading from object: %s object: %s: %v", fName, err)
+		return stat, fmt.Errorf("Error reading from object: %s object: %s: %v", fName, err)
 	}
 
 	data, err := snappy.Decode(nil, cdata)
 	if err != nil {
-		return fmt.Errorf("Error decompressing data: %s object: %s: %v", fName, err)
+		return stat, fmt.Errorf("Error decompressing data: %s object: %s: %v", fName, err)
 	}
 
 	//im := &scimage.GrayU8{Pix: data, Stride: 400, Rect: image.Rect(0, 0, 400, 400), Min: 0, Max: 100, NoData: 255}
-	im := &scimage.GrayU8{Pix: data, Stride: 400, Rect: image.Rect(0, 0, 400, 400), Min: 1, Max: 4, NoData: 0}
+	im := &scimage.GrayU8{Pix: data, Stride: 400, Rect: image.Rect(0, 0, 400, 400), Min: uint8(layer.MinVal), Max: uint8(layer.MaxVal), NoData: uint8(layer.NoData)}
 	rIn := &raster.Raster{im, tileCov}
 
-	fmt.Println("before:", im.Mean())
-	fmt.Println("before:", im.Sum())
 	rIn.CropPolygon(*poly)
-	fmt.Println("after:", im.Mean())
-	fmt.Println("after:", im.Sum())
 
-	out, err := os.Create(fName + ".png")
-        if err != nil {
-		return err
-        }
+	for _, val := range im.Pix {
+		if val != im.NoData {
+			stat.Sum += float64(val)
+			stat.Count += 1
+		}
+	}
 
-	err = png.Encode(out, im)
-        if err != nil {
-		return err
-        }
-
-
-	return nil
+	return stat, nil
 
 }
 
 func DrillDEA(layer Layer, poly geometry.Polygon) (string, error) {
 
 	cov := proj4go.Coverage{BoundingBox: poly.BoundingBox(), Proj4: geographic}
-	covGDA94, err := cov.Transform(gda94)
+	covNat, err := cov.Transform(layer.Proj4)
 	if err != nil {
 		return "", err
 	}
 
-	minX := int(math.Floor(covGDA94.BoundingBox.Min.X / 1e4))
-	minY := int(math.Floor(covGDA94.BoundingBox.Min.Y / 1e4))
-	maxX := int(math.Ceil(covGDA94.BoundingBox.Max.X / 1e4))
-	maxY := int(math.Ceil(covGDA94.BoundingBox.Max.Y / 1e4))
+	minX := int(math.Floor(covNat.BoundingBox.Min.X / 1e4))
+	minY := int(math.Floor(covNat.BoundingBox.Min.Y / 1e4))
+	maxX := int(math.Ceil(covNat.BoundingBox.Max.X / 1e4))
+	maxY := int(math.Ceil(covNat.BoundingBox.Max.Y / 1e4))
 
-	fmt.Prinlnt("BBox:", minX, maxX, minY, maxY)
 	level := 0
 	tileStep := (1 << level)
 
@@ -102,10 +94,8 @@ func DrillDEA(layer Layer, poly geometry.Polygon) (string, error) {
 	y0 := (minY+100)/tileStep*tileStep - 100
 	y1 := (maxY+100)/tileStep*tileStep - 100
 
-	fmt.Prinlnt("Tiles:", x0, x1, y0, y1)
-
 	g := proj4go.ProjGeometry{&poly, geographic}
-	g, err = g.Transform(gda94)
+	g, err = g.Transform(layer.Proj4)
 	if err != nil {
 		fmt.Println("Error reprojecting tile")
 		return "", err
@@ -118,7 +108,7 @@ func DrillDEA(layer Layer, poly geometry.Polygon) (string, error) {
 	for x := x0; x <= x1; x += tileStep {
 		for y := y1; y >= y0; y -= tileStep {
 			wg.Add(1)
-			go DrillTile(x, y, level, p, &wg)
+			go DrillTile(x, y, level, p, layer, &wg)
 		}
 	}
 
@@ -127,35 +117,41 @@ func DrillDEA(layer Layer, poly geometry.Polygon) (string, error) {
 	return fmt.Sprintf("%d, %d, %d, %d", x0, x1, y0, y1), nil
 }
 
-func WarpTile(x, y, level, year int, out *raster.Raster, wg *sync.WaitGroup) error {
+func WarpTile(x, y, level, year int, out *raster.Raster, layer Layer, wg *sync.WaitGroup) error {
 	defer wg.Done()
 
-	tileStep := (1 << level)
-	tileCov := proj4go.Coverage{BoundingBox: geo.BBox(float64(x)*1e4, float64(y-tileStep)*1e4, float64(x+tileStep)*1e4, float64(y)*1e4), Proj4: gda94}
-	fmt.Println(tileCov)
-
-	fName := fmt.Sprintf(tileName, x, y, level)
-	fmt.Println(fName)
-	r, err := os.Open(fName)
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
 	if err != nil {
-		fmt.Println(fName, ": not found")
+		return err
+	}
+
+	bkt := client.Bucket(bucketName)
+
+
+	tileStep := (1 << level)
+	tileCov := proj4go.Coverage{BoundingBox: geo.BBox(float64(x)*1e4, float64(y-tileStep)*1e4, float64(x+tileStep)*1e4, float64(y)*1e4), Proj4: layer.Proj4}
+
+	objName := fmt.Sprintf(tileName, x, y, level)
+	rc, err := bkt.Object(objName).NewReader(ctx)
+	if err != nil {
+		fmt.Println(objName, ": not found")
 		return nil
 	}
 
-	fmt.Println("processing: ", fName)
-	cdata, err := ioutil.ReadAll(r)
-	r.Close()
+	cdata, err := ioutil.ReadAll(rc)
+	rc.Close()
 	if err != nil {
-		return fmt.Errorf("Error reading from object: %s object: %s: %v", fName, err)
+		return fmt.Errorf("Error reading from object: %s object: %s: %v", objName, err)
 	}
 
 	data, err := snappy.Decode(nil, cdata)
 	if err != nil {
-		return fmt.Errorf("Error decompressing data: %s object: %s: %v", fName, err)
+		return fmt.Errorf("Error decompressing data: %s object: %s: %v", objName, err)
 	}
 
 	//im := &scimage.GrayU8{Pix: data, Stride: 400, Rect: image.Rect(0, 0, 400, 400), Min: 0, Max: 100, NoData: 255}
-	im := &scimage.GrayU8{Pix: data, Stride: 400, Rect: image.Rect(0, 0, 400, 400), Min: 1, Max: 4, NoData: 0}
+	im := &scimage.GrayU8{Pix: data, Stride: 400, Rect: image.Rect(0, 0, 400, 400), Min: uint8(layer.MinVal), Max: uint8(layer.MaxVal), NoData: uint8(layer.NoData)}
 	rIn := &raster.Raster{im, tileCov}
 	out.Warp(rIn)
 
@@ -164,6 +160,8 @@ func WarpTile(x, y, level, year int, out *raster.Raster, wg *sync.WaitGroup) err
 }
 
 func GenerateDEATile(layer Layer, width, height int, bbox geometry.BoundingBox, date time.Time) (*image.Paletted, error) {
+
+
 	img := scimage.NewGrayU8(image.Rect(0, 0, width, height), 1, 4, 0)
 	cov := proj4go.Coverage{BoundingBox: bbox, Proj4: webMerc}
 	rMerc := &raster.Raster{Image: img, Coverage: cov}
@@ -186,7 +184,7 @@ func GenerateDEATile(layer Layer, width, height int, bbox geometry.BoundingBox, 
 
 	tileStep := (1 << level)
 
-	covGDA94, err := cov.Transform(gda94)
+	covGDA94, err := cov.Transform(layer.Proj4)
 	if err != nil {
 		return nil, err
 	}
@@ -206,7 +204,7 @@ func GenerateDEATile(layer Layer, width, height int, bbox geometry.BoundingBox, 
 	for x := x0; x <= x1; x += tileStep {
 		for y := y1; y >= y0; y -= tileStep {
 			wg.Add(1)
-			go WarpTile(x, y, level, date.Year(), rMerc, &wg)
+			go WarpTile(x, y, level, date.Year(), rMerc, layer, &wg)
 		}
 	}
 
---
+++ rastreader/stat.go
@@ -0,0 +1,7 @@
+package rastreader
+
+
+type Stat struct {
+	Sum float64
+	Count float64
+}
